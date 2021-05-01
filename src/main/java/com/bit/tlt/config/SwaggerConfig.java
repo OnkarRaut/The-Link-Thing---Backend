3eb42428e8d41c03c115f97dee83e9c6343a6ab1
panda <rautonkar3@gmail.com>
2021-05-01 15:29:10 +0530

    initial commit

---
+++ src/main/java/com/bit/tlt/config/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.bit.tlt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@ConditionalOnProperty(name="swagger.api.enabled", havingValue = "true")
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("${swagger.api.title}")
+    private String apiTitle;
+
+    @Value("${swagger.api.version}")
+    private String apiVersion;
+
+    @Value("${swagger.api.description}")
+    private String apiDescription;
+
+    @Value("${swagger.docs.contact.name}")
+    private String contactName;
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .contact(contact())
+                .version(apiVersion)
+                .title(apiTitle)
+                .description(apiDescription)
+                .build();
+
+    }
+
+    private Contact contact() {
+        return new Contact(contactName, null, null);
+    }
+
+}
